8ea1b123b00b7e48318937ca16a12f10b1688557
mdsaibhossain <saibhossain5@gmail.com>
2025-08-12 02:11:44 +0600

    db connection

---
+++ langgraph_app/db.py
@@ -0,0 +1,26 @@
+import pymysql
+from pymysql import cursors
+
+def get_connection():
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="smart_hub",
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+def fetch_faq_answer(query_text):
+    db = get_connection()
+    with db.cursor() as cursor:
+        cursor.execute("SELECT answer FROM faq WHERE question LIKE %s LIMIT 1", ("%" + query_text + "%",))
+        result = cursor.fetchone()
+    db.close()
+    return result['answer'] if result else None
+
+def log_ai_response(user_query, ai_response):
+    db = get_connection()
+    with db.cursor() as cursor:
+        cursor.execute("INSERT INTO ai_logs (user_query, ai_response) VALUES (%s, %s)", (user_query, ai_response))
+        db.commit()
+    db.close()
