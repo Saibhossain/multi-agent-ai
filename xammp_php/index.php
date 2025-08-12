388af3f302efcd9c92e2e2426ca30124327df36b
mdsaibhossain <saibhossain5@gmail.com>
2025-08-12 20:09:39 +0600

    index page

---
+++ xammp_php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Smart Hub Query</h2>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+  Query: <input type="text" name="query"><br><br>
+  File: <input type="file" name="file"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
